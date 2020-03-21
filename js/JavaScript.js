baab6dfddc18506f384875d5074142b8a54586a7
Thomas Petrangelo <thpetran@syr.edu>
2020-03-21 18:38:44 -0400

    CSS and JS now working and files are created in projects

---
+++ js/JavaScript.js
@@ -0,0 +1,15 @@
+﻿$('.toggle').click(function (e) {
+    e.preventDefault();
+
+    var $this = $(this);
+
+    //this is the close section
+    if ($this.next().hasClass('show') ) {
+        $this.next().removeClass('show');
+        $this.next().slideUp(350);
+    } else { //This is the open section
+        $this.parent().parent().find('li div .inner').removeClass('show');
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
+    }
+});
